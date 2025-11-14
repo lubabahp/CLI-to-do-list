4009df342677b0ca9114ee95ccb2dc66e1418698
Lubabah Pervaj <lubabah.p@outlook.com>
2025-11-14 16:50:10 +0000

    Implement menu display and user input functions
    Added functions to display menu and get user input for tasks.

--- todo/menu.py
+++ todo/menu.py
@@ -1,1 +1,13 @@
 # functions that show menu and get user input
+
+def show_menu():   # this is our menu, we can call the funtion so the user sees their options.
+  print("Your To-do List")
+  print("1. View tasks")
+  print("2. Add tasks")
+  print("3. Complete task")
+  print("4. Delete task")
+  print("5. Exit")
+
+def user_picks_option():
+  return input("Pick an option from 1 to 5: ")
+  # return means the input goes back to the function and will come when the function is called
