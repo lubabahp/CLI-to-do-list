1517bfa52f2adf4c4de604eb13462ebdbc10c5a9
Lubabah Pervaj <lubabah.p@outlook.com>
2025-11-14 17:06:10 +0000

    Enhance comments and define task management functions
    Updated comments for clarity and added task management functions.

--- todo/tasks.py
+++ todo/tasks.py
@@ -1,1 +1,17 @@
-# functions that manage the tasks
+# functions that manage the tasks, the task-related functions for the application.
+
+def add_task(tasks):   # this function will as it says, add tasks when called
+  task = input("Enter a new task")   # this is the argument, which is the value put in the function
+
+# i didn't know functions can literally be that small, i learnt now the point of functions is so we don't need to write the same code over and over
+
+def view_tasks(tasks):
+  pass   # placeholder for future code, it basically tells python to ignore the function so an error message doesn't show up
+
+def complete_tasks(tasks):
+  pass
+
+def delete_tasks(tasks):
+  
+
+
