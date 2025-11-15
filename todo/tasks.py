ebb78bc45541265cad6d8a2428744b6abad5458f
Lubabah Pervaj <lubabah.p@outlook.com>
2025-11-15 23:48:42 +0000

    Update tasks.py
    cleaned up some code

--- todo/tasks.py
+++ todo/tasks.py
@@ -10,7 +10,6 @@ def add_task(tasks):   # this function will as it says, add tasks when called
 # "pass" placeholder for future code, it basically tells python to ignore the function so an error message doesn't show up
 
 def view_tasks(tasks):
-  check length of list
   if len(tasks) == 0:
     print("There are no tasks.")
   else:
@@ -36,10 +35,10 @@ def delete_tasks(tasks):
   if len(tasks) == 0:
     return
   num = int(input("Enter task number to delete. "))
-    if 1 <= num <= len(tasks):
-       removed = tasks.pop(num - 1)   # .pop() removes it. minus one because python starts counting from 0, but the list visible to the user starts from 1. so it's to ensure it removes the correct task.
-       print(f"Deleted {removed}.")    # f strings are called that cause they are used for formatting
-    else:
-      print("Invalid task number")
+  if 1 <= num <= len(tasks):
+     removed = tasks.pop(num - 1)   # .pop() removes it. minus one because python starts counting from 0, but the list visible to the user starts from 1. so it's to ensure it removes the correct task.
+     print(f"Deleted {removed}.")    # f strings are called that cause they are used for formatting
+  else:
+     print("Invalid task number")
 
-# paramter is the variable in the (). argument is the value that are sent to the function when it's called.
+# parameter is the variable in the (). argument is the value that are sent to the function when it's called.
